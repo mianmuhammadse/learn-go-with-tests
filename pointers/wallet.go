99a27ac8e8f5959a91fd729578f95b002b13cc14
Mian Muhammad <mianmuhammad660@gmail.com>
2024-09-02 19:40:52 +0500

    NEW: pointers, nill, error

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,16 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
 
 type Wallet struct{
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
